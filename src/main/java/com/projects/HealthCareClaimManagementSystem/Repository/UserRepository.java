bc4a0d3b718944363c9be4e6a583d43e910cc9b4
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-28 17:01:06 +0530

    created user repository

---
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.projects.HealthCareClaimManagementSystem.Repository;
+
+import com.projects.HealthCareClaimManagementSystem.Entitiy.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByUserEmail(String email);
+}
