0c4aa3334430d6c716560ff7939db82f17f2cda1
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-29 21:51:11 +0530

    created AuthenticatedUserDto

---
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Exception/AuthenticationFailedException.java
@@ -0,0 +1,7 @@
+package com.projects.HealthCareClaimManagementSystem.Exception;
+
+public class AuthenticationFailedException extends CustomException {
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
+}
