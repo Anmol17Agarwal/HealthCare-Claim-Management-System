8bc28497931d310e0b6f587f845fdbc705fee1d3
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-29 21:51:54 +0530

    created AuthenticationFailedException

--- src/main/java/com/projects/HealthCareClaimManagementSystem/Exception/AuthenticationFailedException.java
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Exception/AuthenticationFailedException.java
@@ -1,5 +1,6 @@
 package com.projects.HealthCareClaimManagementSystem.Exception;
 
+
 public class AuthenticationFailedException extends CustomException {
     public AuthenticationFailedException(String message) {
         super(message);
