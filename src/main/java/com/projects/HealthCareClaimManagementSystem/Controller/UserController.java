3491d1e9dd02865968711f09b0fd9f9776b5c646
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-29 21:48:01 +0530

    optimized controller with Response Entity and CustomResponse with try and catch

--- src/main/java/com/projects/HealthCareClaimManagementSystem/Controller/UserController.java
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Controller/UserController.java
@@ -1,26 +1,36 @@
 package com.projects.HealthCareClaimManagementSystem.Controller;
 
+import com.projects.HealthCareClaimManagementSystem.Dto.LoginDto;
+import com.projects.HealthCareClaimManagementSystem.Dto.UserDto;
 import com.projects.HealthCareClaimManagementSystem.Entitiy.UserEntity;
 import com.projects.HealthCareClaimManagementSystem.Service.UserService;
+import com.projects.HealthCareClaimManagementSystem.Utility.CustomResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public UserEntity register(@RequestBody UserEntity user){
-        userService.register(user);
-        return user;
+    public ResponseEntity<CustomResponse<UserDto>> register(@RequestBody UserEntity user){
+            UserDto userDto = userService.register(user);
+            return ResponseEntity.ok(CustomResponse.success("User Registered Successfully", userDto));
     }
 
+    @GetMapping("/getAll")
+    public ResponseEntity<CustomResponse<List<UserDto>>> getAllUsers(){
+        List<UserDto> users = userService.getAllUsers();
+        return ResponseEntity.ok(CustomResponse.success("Users fetched successfully", users));
+    }
     @PostMapping("/login")
-    public String login(@RequestBody UserEntity users){
-        return userService.verify(users);
-
+    public ResponseEntity<CustomResponse<LoginDto>> login(@RequestBody UserEntity users){
+        LoginDto loginDto = userService.verify(users);
+        return ResponseEntity.ok(CustomResponse.success("Login successful", loginDto));
     }
 }
