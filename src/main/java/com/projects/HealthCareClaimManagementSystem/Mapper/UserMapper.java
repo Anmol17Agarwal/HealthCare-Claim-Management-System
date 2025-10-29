501d2a65775d0bed488cd9d40c26d1944ab6f580
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-29 21:49:42 +0530

    created UserMapper

---
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.projects.HealthCareClaimManagementSystem.Mapper;
+
+import com.projects.HealthCareClaimManagementSystem.Dto.RoleDto;
+import com.projects.HealthCareClaimManagementSystem.Dto.UserDto;
+import com.projects.HealthCareClaimManagementSystem.Entitiy.RoleEntity;
+import com.projects.HealthCareClaimManagementSystem.Entitiy.UserEntity;
+
+import java.util.stream.Collectors;
+
+public class UserMapper {
+
+    public static UserEntity toEntity(UserDto dto) {
+        return UserEntity.builder()
+                .username(dto.getUsername())
+                .fullName(dto.getFullName())
+                .userEmail(dto.getUserEmail())
+                .roles(dto.getRoles().stream()
+                        .map(role -> RoleEntity.builder().name(role.getName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+
+    public static UserDto toDTO(UserEntity entity) {
+        return UserDto.builder()
+                .userId(entity.getUser_id())
+                .username(entity.getUsername())
+                .fullName(entity.getFullName())
+                .userEmail(entity.getUserEmail())
+                .roles(entity.getRoles().stream()
+                        .map(role -> RoleDto.builder().name(role.getName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+}
