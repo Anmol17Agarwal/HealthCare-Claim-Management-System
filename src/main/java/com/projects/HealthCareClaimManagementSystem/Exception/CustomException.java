bb24bbb2bc1a90f4c07955657bfcd0f05f4ed491
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-29 21:43:50 +0530

    created CustomException

---
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.projects.HealthCareClaimManagementSystem.Exception;
+
+public class CustomException extends RuntimeException{
+    public CustomException(String message){
+        super(message);
+    }
+}
