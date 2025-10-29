12086afb14bda773c864b3d86a31e76f6d8f1fcb
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-29 21:45:12 +0530

    created ResourceNotFoundException

---
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.projects.HealthCareClaimManagementSystem.Exception;
+
+public class ResourceNotFoundException extends CustomException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
