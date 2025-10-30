fc7012361979bf9971e9215693f62328cb6e2a1f
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-31 00:18:43 +0530

    added @Repository annotation

--- src/main/java/com/projects/HealthCareClaimManagementSystem/Repository/UserRepository.java
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.projects.HealthCareClaimManagementSystem.Repository;
 
 import com.projects.HealthCareClaimManagementSystem.Entitiy.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     boolean existsByUsername(String username);
