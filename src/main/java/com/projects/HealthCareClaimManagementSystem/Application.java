1f1fd663e7a12347c133bcfba29b9d61378671d4
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-31 02:24:03 +0530

    added @EnableScheduling annotation

--- src/main/java/com/projects/HealthCareClaimManagementSystem/Application.java
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Application.java
@@ -2,10 +2,10 @@ package com.projects.HealthCareClaimManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
