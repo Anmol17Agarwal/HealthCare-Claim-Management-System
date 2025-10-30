ce8f7bf3c875a83f6030936865ba6a8a6d006f6b
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-31 00:20:05 +0530

    added updated at field

--- src/main/java/com/projects/HealthCareClaimManagementSystem/Entitiy/PatientEntity.java
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Entitiy/PatientEntity.java
@@ -64,4 +64,7 @@ public class PatientEntity {
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
