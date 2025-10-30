b56670307de0ebe27837e7fc45d63e5ee6c4e8a5
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-30 16:02:44 +0530

    created ClaimController

---
+++ src/main/java/com/projects/HealthCareClaimManagementSystem/Controller/ClaimController.java
@@ -0,0 +1,48 @@
+package com.projects.HealthCareClaimManagementSystem.Controller;
+
+import com.projects.HealthCareClaimManagementSystem.Dto.ClaimDto;
+import com.projects.HealthCareClaimManagementSystem.Service.ClaimService;
+import com.projects.HealthCareClaimManagementSystem.Utility.CustomResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/claims")
+public class ClaimController {
+
+    @Autowired
+    ClaimService claimService;
+
+    @PostMapping
+    private ResponseEntity<CustomResponse<ClaimDto>> createClaim(@RequestBody ClaimDto claimDto){
+         ClaimDto claim =   claimService.createClaim(claimDto);
+         return ResponseEntity.ok(CustomResponse.success("Claim has been created successfully",claim));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomResponse<ClaimDto>> getClaimById(@PathVariable Long id){
+            ClaimDto claim = claimService.getClaimById(id);
+            return ResponseEntity.ok(CustomResponse.success("Claim retrieved successfully",claim));
+    }
+
+    @GetMapping
+    public ResponseEntity<CustomResponse<List<ClaimDto>>> getAllClaims(){
+            List<ClaimDto> claimDtos = claimService.getAllClaims();
+            return ResponseEntity.ok(CustomResponse.success("Claims retrieved successfully",claimDtos));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomResponse<ClaimDto>> updateClaim(@PathVariable Long id, @RequestBody ClaimDto claimDto){
+            ClaimDto updatedClaim = claimService.updateClaim(id,claimDto);
+            return ResponseEntity.ok(CustomResponse.success("Claim updated Successfully",updatedClaim));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CustomResponse<String>> deleteClaim(@PathVariable Long id){
+            claimService.deleteClaim(id);
+            return ResponseEntity.ok(CustomResponse.success("Claim Deleted with ID: "+id,null));
+    }
+}
